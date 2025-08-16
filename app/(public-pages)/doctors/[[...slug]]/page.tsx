3200c7c2f4f21fe08d0cfebe542ee0fb34994366
asjadBuilds <asjadqureshi77@gmail.com>
2025-08-16 16:00:57 +0500

    fixed params in doctors page

--- app/(public-pages)/doctors/[[...slug]]/page.tsx
+++ app/(public-pages)/doctors/[[...slug]]/page.tsx
@@ -1,33 +1,37 @@
 import DoctorListing from '@/components/specific/DoctorListing'
 import LocationFilter from '@/components/specific/LocationFilter'
-import getQueryClient from '@/utils/queryClient'
 import React from 'react'
 
-type Props = {
-    searchParams: {
-        country: string,
-        state: string,
-        city: string,
-        search: string
-    },
-    params: {
-        slug: string[]
-    }
-}
-const page = async({ params, searchParams }: Props) => {
-    let id, title;
-    let doctorName
-    if (params.slug) {
-        [title, id] = await params.slug;
-        doctorName = decodeURIComponent(title)
-    }
-    const { country, state, city, search } = await searchParams;
-    const queryDoctor = decodeURIComponent(search)
+type PageProps = {
+  params: {
+    slug?: string[];
+  };
+  searchParams: {
+    country?: string;
+    state?: string;
+    city?: string;
+    search?: string;
+  };
+};
+const page = async({ params, searchParams }: PageProps) => {
+    let id: string | undefined;
+  let doctorName: string | undefined;
+
+  if (params.slug && params.slug.length >= 2) {
+    const [title, categoryId] = params.slug;
+    doctorName = decodeURIComponent(title);
+    id = categoryId;
+  }
+     const queryDoctor = searchParams.search ? decodeURIComponent(searchParams.search) : "";
     return (
         <div className='flex flex-col items-center gap-8'>
             <h1 className='text-3xl font-bold max-md:text-center'>{doctorName || queryDoctor}</h1>
             <LocationFilter />
-            <DoctorListing categoryId={id!} country={country} city={city} query={search} />
+            <DoctorListing 
+            categoryId={id || ""}
+        country={searchParams.country || ""}
+        city={searchParams.city || ""}
+        query={searchParams.search || ""} />
         </div>
     )
 }
