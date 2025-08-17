546c761cb004c5b465bdba4ca2f87b6c643f1e34
asjadBuilds <asjadqureshi77@gmail.com>
2025-08-17 11:10:47 +0500

    refactored params structure in confirm payment

--- app/(public-pages)/confirmPayment/[...slug]/page.tsx
+++ app/(public-pages)/confirmPayment/[...slug]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { Button } from "@/components/ui/button"
 import { confirmAppointment } from "@/services/paymentApi"
-import { useRouter } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import { useMutation } from "@tanstack/react-query"
 
-const page = async({ params }: { params: Promise<{ slug: string[] }> }) => {
+const page = () => {
     const router = useRouter();
-    const {slug} = await params;
+    const params = useParams<{ slug?: string[] }>();
+    const slug = params?.slug ?? []
     const [doctorId, doctorName, fee, appointmentId] =  slug;
     const decodeDocName = decodeURIComponent(doctorName)
     const {mutate} = useMutation({
@@ -16,7 +17,7 @@ const page = async({ params }: { params: Promise<{ slug: string[] }> }) => {
         }
     })
   return (
-    <div className="h-[calc(100vh-68px)] flex flex-col gap-3 items-center justify-center text-center">
+    <div className="h-[calc(100vh-68px)] flex flex-col gap-3 items-center justify-center text-center bg-background">
         <h1 className="font-sans text-3xl font-bold">Confirm Your Appointment</h1>
         <p className="font-mono text-neutral-400">Confirm Your Payment to book consultation</p>
         <Button onClick={()=>mutate({doctorId, doctorName:decodeDocName, fee, appointmentId})}>Confirm Payment</Button>
--- app/(public-pages)/paymentSuccess/page.tsx
+++ app/(public-pages)/paymentSuccess/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"
 const page = () => {
   const router = useRouter();
   return (
-     <div className="flex flex-col items-center justify-center gap-3 min-h-screen text-center">
+     <div className="flex flex-col items-center justify-center gap-3 min-h-screen text-center bg-background">
       <h1 className="text-3xl font-bold font-sans text-primary">Payment Successful! 🎉</h1>
       <p className="font-mono text-neutral-400">Thank you for your booking. Your appointment has been confirmed.</p>
       <Button onClick={()=>router.push('/')}>Continue</Button>
